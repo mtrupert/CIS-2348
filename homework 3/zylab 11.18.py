a69fa179ffaecab52171f35211af1f6e2a62c355
mtrupert <89813721+mtrupert@users.noreply.github.com>
2021-11-10 23:47:59 -0600

    Delete homework 3 directory

--- homework 3/zylab 11.18.py
+++
@@ -1,4 +0,0 @@
-user_input = input()
-my_list = [int(i) for i in user_input.split() if (int(i)>=0)]
-my_list.sort()
-[print(i, end=' ') for i in my_list]
--- homework 3/zylab 11.22.py
+++
@@ -1,6 +0,0 @@
-list = input()
-text = list.split()
-
-for word in text:
-        freq = text.count(word)
-        print(word, freq)
